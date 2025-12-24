c0e3bd4ce8befcff1118d14f51e069eca7813bff
Mohammed Salem <mohamedsalem462006@gmail.com>
2025-12-24 08:24:19 +0200

    Add files via upload

---
+++ OOP/Assignment1/assignment1.java
@@ -0,0 +1,6 @@
+public class assignment1 {
+    public static void main(String[] args) {
+        // a program to pring hello world
+        System.out.println("Hello World");
+    }
+}
---
+++ OOP/Assignment1/assignment2.java
@@ -0,0 +1,8 @@
+public class assignment2 {
+    public static void main(String[] args) {
+        // Print personal information
+        System.out.println("Name: Sherlock Holmes");
+        System.out.println("Address: 221B, Baker Street, London, England");
+        System.out.println("Tel. No.: 0123456789");
+    }
+}
